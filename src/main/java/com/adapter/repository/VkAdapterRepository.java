bb16224906c67aa7a65b87b79feace24a0913556
gleb <gleboshkay@list.ru>
2024-09-10 22:57:02 +0500

    1) Edit Like and Dislike endpoints

--- src/main/java/com/adapter/repository/VkAdapterRepository.java
+++ src/main/java/com/adapter/repository/VkAdapterRepository.java
@@ -1,7 +1,9 @@
 package com.adapter.repository;
 
 import com.adapter.model.DatingProfile;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface VkAdapterRepository extends JpaRepository<DatingProfile, Long> {
@@ -9,5 +11,8 @@ public interface VkAdapterRepository extends JpaRepository<DatingProfile, Long> 
     @Query("SELECT EXISTS(SELECT p FROM DatingProfile p WHERE p.profileId = :profileId)")
     boolean isDatingProfileExist(long profileId);
 
-//    void updateDatingProfile(long profileId);
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM DatingProfile p WHERE p.profileId = :profileId")
+    void deleteByProfileId(long profileId);
 }
